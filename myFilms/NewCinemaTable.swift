1e16634a5b805afe41bff8eef51fc966c7c227dd
Max Palehin <ars_kit@icloud.com>
2020-04-06 21:24:22 +0300

    добавил realm

--- myFilms/NewCinemaTable.swift
+++ myFilms/NewCinemaTable.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class NewCinemaTable: UITableViewController {
     
+    @IBOutlet weak var imageOfPicking: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
     }
@@ -19,19 +20,27 @@ class NewCinemaTable: UITableViewController {
     //MARK: Table view delegate
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         if indexPath.row == 0{
+            
+            
             let actionSheet = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
             
             let camera = UIAlertAction(title: "Camera", style: .default) { _ in
                 self.choseImagePicker(source: .camera)
             }
             
+            
+            
             let photo = UIAlertAction(title: "Photo", style: .default) { _ in
                 self.choseImagePicker(source: .photoLibrary)
             }
+
+           
             let cancel = UIAlertAction(title: "Cancel", style: .cancel)
             actionSheet.addAction(camera)
             actionSheet.addAction(photo)
             actionSheet.addAction(cancel)
+            
+            present(actionSheet, animated: true )
         }else{
             view.endEditing(true)
         }
@@ -49,14 +58,22 @@ extension NewCinemaTable:UITextFieldDelegate{
 }
 
 //MARK: Work with image
-extension NewCinemaTable{
+extension NewCinemaTable:UIImagePickerControllerDelegate,UINavigationControllerDelegate{
     func choseImagePicker(source:UIImagePickerController.SourceType){
         if UIImagePickerController.isSourceTypeAvailable(source){
             let imagePicker = UIImagePickerController()
+            imagePicker.delegate = self
             imagePicker.allowsEditing = true
             imagePicker.sourceType = source
             present(imagePicker,animated: true)
         }
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey: Any]) {
+        imageOfPicking.image = info[.editedImage] as? UIImage
+        imageOfPicking.contentMode = .scaleAspectFill
+        imageOfPicking.clipsToBounds = true
+        dismiss(animated: true)
     }
     
 }
